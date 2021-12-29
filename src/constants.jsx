e90b9f690a1c4e6e9bf33b1fe8b1a146b32c303c
tomeck <dr.teck@gmail.com>
2021-12-29 16:49:49 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0xF7ABDA61768E2Bc854107994588f1239A4903C26';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
